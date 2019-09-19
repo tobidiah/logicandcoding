f839ba78b74b6dc105f542262967d775d0b87dc9
tobidiah <54414550+tobidiah@users.noreply.github.com>
2019-09-19 10:35:00 -0700

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,34 @@
+//Java class ForLoops.Java
+
+/**
+ * Skeleton code or the for loops practice day.
+ * 
+ * @author Roy Chancellor
+ * @version 2/26/2019
+ */
+public class ForLoops
+{
+    public static void main( String args[] ) {
+        //variable declarations
+        String p = "The quick brown fox jumped over the lazy dog";
+        System.out.println(p);
+        System.out.println("I tried, I don't know how, this frustrates me a smidge, I'm sorry I failed you");
+        
+        int y = 99;
+        for (int x = y; x >= 1; x = x - 2) {
+            System.out.println(x);
+        } 
+        
+       
+        int i, cube;
+        int n = 1;
+        for (i = n; i <= 10; i = i + 1) {
+            cube = i * i * i;
+            System.out.println(i + "\t" + cube);
+        
+        //for loop to perform the task
+    }
+    
+    
+    }  //end main
+}  //end ForLoops class
